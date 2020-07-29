caa992aa663ccb91399d2617515930b70ed1fdc0
Robert Alex Crouch <acrouch06@gmail.com>
2020-07-29 13:04:15 -0500

    formatting

--- models/book.js
+++ models/book.js
@@ -1,41 +1,41 @@
 module.exports = function(sequelize, DataTypes) {
-    var Book = sequelize.define("Book", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      author: {
-        type: DataTypes.STRING,
-        allowNull: false,
+  var Book = sequelize.define("Book", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
         len: [1]
-      },
-      publishDate: {
-          type: DataTypes.DATE,
-          allowNull: true
-      },
-      genre: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      synopsis: {
-          type: DataTypes.TEXT,
-          allowNull: false
-      },
-      hasRead: {
-          type: DataTypes.BOOLEAN
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    },
+    publishDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    synopsis: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    hasRead: {
+      type: DataTypes.BOOLEAN
+    }
+  });
+
+  Book.associate = function(models) {
+    Book.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
       }
     });
-  
-    Book.associate = function(models) {
-      Book.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-  
-    return Book;
   };
+
+  return Book;
+};
