330d79e98ec883d477ec71f42501bea719702576
Nakwon Yoon <yoonnw1000@gmail.com>
2020-07-29 19:50:06 -0500

    adding some script

--- public/js/book.js
+++ public/js/book.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
-
-
-
+    const submit = $("#searchBtn");
     
+submit.on("click", function(event){
+    event.preventDefault();
+    console.log("=========");
+    const searchInput = $(".searchInput").val().trim();
+
+        serachByTitle(searchInput);
+    });
+
+
+
+function serachByTitle(searchInput) {
+    const key = "kfqIZ6fbDX5FN0hEnk62w";
+    let url = "https://www.goodreads.com/book/title.xml?key="+ key +"&title=" + searchInput;
+  $.ajax({
+      type: "GET",
+      url : url
+  }).then((response) => {
+   
+    console.log(url);
+    console.log(response);
+  })
+};
+
+function searchByAuthor() {
+
+}
+
 });
