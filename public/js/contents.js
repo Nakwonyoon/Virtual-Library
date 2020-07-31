8ca8805b1cf8eadaecb75764778208b75bf34a09
Nakwon Yoon <yoonnw1000@gmail.com>
2020-07-31 13:01:08 -0500

    delete js file

--- public/js/contents.js
+++ public/js/contents.js
@@ -11,25 +11,11 @@ $(document).ready(function () {
 
   // Will search a book of title, author...etc and bring put the data into cardDeck function.
   function serachByTitle(searchInput) {
-    // const key = "kfqIZ6fbDX5FN0hEnk62w";
-    // let url = "https://www.goodreads.com/book/title.xml?key=" + key + "&title=" + searchInput;
-    // $.ajax({
-    //   type: "GET",
-    //   url: url
-    // }).then(function (response) {
-    //   console.log(url);
-    //   console.log(response)
-      $.get("/goodreads", function () {
-        const bookData = {
-          title: response.title,
-          author: response.author,
-          publishDate: response.publishDate,
-          genre: response.genre,
-          synopsis: response.synopsis,
-          hasRead: response.hasRead
-        }
-      }).then((bookData) => {
-        res.send(cardDeck(bookData));
+      $.get("/goodreads", function (response) {
+        console.log(response);
+
+      }).then((response) => {
+        res.send(cardDeck(response));
       })
       .catch((err) => console.log(err));
 };
@@ -39,16 +25,15 @@ $(document).ready(function () {
 //  need  call lib  and create function by using carddeck.
 
   // creating cardDeck 
-function cardDeck(bookData) {
+function cardDeck(response) {
     cardForm.append($("#books"));
     const cardForm = ` 
     <div style="width: 24rem;" class="card results-card">
     <div class="card-body">
-    <h5 class="card-title">  ${bookData.title} </h5>
-    <h6 class="card-text"> ${bookData.author}  </h6>
-    <p> ${bookData.genre}</p>
-    <p> ${bookData.synopsis}</p>
-    <p> ${bookData.hasRead}</p>
+    <img src="${response.img}" alt="cover">
+    <h5 class="card-title">  ${response.title} </h5>
+    <h6 class="card-text"> ${response.author}  </h6>
+    
     `;
   }
   // Submit the input by click or enter
