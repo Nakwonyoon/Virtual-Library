fe9e5d49845b71ce4641a15c1131d53ddf54c148
Alex Crouch <acrouch06@gmail.com>
2020-08-03 02:48:25 -0500

    fixed route error

--- routes/api-book-routes.js
+++ routes/api-book-routes.js
@@ -67,7 +67,7 @@ module.exports = function(app) {
   app.delete("/api/mybooks/:id", function(req, res) {
     db.Book.destroy({
       where: {
-        id: req.params.id,
+        goodReadsId: req.params.id,
       },
     }).then(function(dbBook) {
       res.json(dbBook);
