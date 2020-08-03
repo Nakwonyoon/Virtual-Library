9c32b3c7638094cbba15afe3390e6d7a6d80cc98
Nakwon Yoon <yoonnw1000@gmail.com>
2020-08-02 23:28:27 -0500

    upadtaing

--- public/js/contents.js
+++ public/js/contents.js
@@ -17,7 +17,7 @@ function serachByTitle(searchInput) {
       var { books } = response;
       console.log(books);
       for (var i = 0; i < books.length; i++) {
-        searchList.push(cardDeckSearch(books[i]));
+        searchList.push(cardDeck(books[i]));
       }
     }).catch((err) => console.log(err));
   };
@@ -41,86 +41,92 @@ function serachByTitle(searchInput) {
     }).catch((err) => console.log(err));
   }
   // add to the Quelist
-  $(".addQue").on("click", function (event) {
+  $("#searchResult").on("click", $("button.addQue"), async function (event) {
     event.preventDefault();
-    alert("asd")
-    var newQue = {
-      covers: data.covers,
-      title: data.title,
-      authors: data.authors
-      }
-      $.post("/api/myqueue", newQue)
-      .then(function(newQue){
-        alert("adding");
-        console.log(newQue);
+    event.stopPropagation();
+    let id;
+    await $.get("/api/user_data").then(function(data) {
+      id = data.id;
+    });
+    let newTitle = event.target.closest(".card-body").querySelector(".card-title");
+    let newAuthor = event.target.closest(".card-body").querySelector(".card-text");
+    let newDate = event.target.closest(".card-body").querySelector(".card-date");
+    let newImage = event.target.closest(".card-body").querySelector(".cover").getAttribute("src");
+    var newBook = {
+      title: newTitle.innerHTML.trim(),
+      author: newAuthor.innerHTML.trim(),
+      publishDate: newDate.innerHTML.trim(),
+      image: newImage,
+      UserId: id
+    }
+    console.log(newBook)
+    $.post("/api/myqueue", newBook, function (data) {
+      console.log(data);
+      alert("added");
     });
   })
+
 //  add to the bookList
   $(".done").on("click", function (event) {
     event.preventDefault();
-    var newQue = {
-    covers: covers,
-    title: title,
-    authors: authors
-    }
-    $.post("/api/mybooks", newQue)
-    .then(function(data){
-      console.log(data);
+    event.stopPropagation();
+    $.post("/api/mybooks").then(function (data) {
+      return rendermyBookList;
     })
-  });
+  })
 
   // delete from myQue
   $(".deleteQue").on("click", function (event) {
     event.preventDefault();
+    event.stopPropagation();
     $.delete("/api/myqueue/:id").then(function (data) {
       return renderQueueList
     });
   })
 //  delete from mybook
-  $(".deleteBook").on("click", function (event) {
+  $(".remove").on("click", function (event) {
     event.preventDefault();
+    event.stopPropagation();
     $.delete("/api/mybooks/:id").then(function (data) {
       return rendermyBookList;
     });
   })
   // creating cardDeck 
-  function cardDeckSearch(data) {
+  function cardDeck(data) {
 
     var cardForm = ` 
-  
     <div style="width: 24rem;" class="card results-card">
     <div class="card-body">
-    <img src="${data.covers}" alt="cover">
+    <img class="cover" src="${data.covers}" alt="cover">
     <h5 class="card-title">  ${data.title} </h5>
-    <h6 class="card-text"> ${data.authors}  </h6>
+    <h6 class="card-text">${data.authors}  </h6>
+    <h6 class="card-date">${data.year}  </h6>
     <button class="addQue">add</button>
-
     `;
     searchList.append($(cardForm));
   }
   function cardDeckOfQList(data) {
     const cardForm = ` 
-  
     <div style="width: 24rem;" class="card results-card">
     <div class="card-body">
-    <img src="${data.covers}" alt="cover">
+    <img class="cover" src="${data.covers}" alt="cover">
     <h5 class="card-title">  ${data.title} </h5>
     <h6 class="card-text"> ${data.authors}  </h6>
+    <h6 class="card-date"> ${data.year}  </h6>
     <button class="done">Done</button> 
     <button class="deleteQue">delete</button> 
-  
     `;
     MyQueueList.append($(cardForm));
   }
   function cardDeckOfBookList(data) {
     const cardForm = ` 
-    
     <div style="width: 24rem;" class="card results-card">
     <div class="card-body">
-    <img src="${data.covers}" alt="cover">
+    <img class="cover" src="${data.covers}" alt="cover">
     <h5 class="card-title">  ${data.title} </h5>
     <h6 class="card-text"> ${data.authors}  </h6>
-    <button class="deleteBook">delete</button> 
+    <h6 class="card-date"> ${data.year}  </h6>
+    <button class="remove">remove</button> 
 
     `;
     MyBookList.append($(cardForm));
@@ -131,7 +137,6 @@ function serachByTitle(searchInput) {
   submit.on("click", function(event) {
     event.preventDefault();
     event.stopPropagation();
-    alert("");
     searchList.show();
     // MyQueueList.hide();
     // MyBookList.hide();
@@ -143,7 +148,6 @@ function serachByTitle(searchInput) {
 
   myQueueBtn.on("click", (event) => {
     event.preventDefault();
-    event.stopPropagation();
     MyQueueList.show();
     MyBookList.hide();
     console.log("asd");
@@ -151,7 +155,6 @@ function serachByTitle(searchInput) {
   });
   myLibraryBtn.on("click", (event) => {
     event.preventDefault();
-    event.stopPropagation();
     MyBookList.show();
     MyQueueList.hide();
     console.log("asd");
