c0b688b89b810e22e147442a2d00ffbea11643d3
Nakwon Yoon <yoonnw1000@gmail.com>
2020-08-02 19:58:12 -0500

    asd

--- public/js/contents.js
+++ public/js/contents.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
   // buttons
   const submit = $("#searchBtn");
   const myQueueBtn = $("#myQ");
@@ -6,64 +7,94 @@ $(document).ready(function () {
   // list
   const searchList = $("#searchResult");
   const MyQueueList = $("#myQueueList");
-  const MyLibraryList = $("#myLibraryList");
+  const MyBookList = $("#myLibraryList");
 
 
   function serachByTitle(searchInput) {
-    $.get("/goodReads", {
-      id: response.books.goodreadsId,
-      title: response.books.title,
-      author: response.books.authors,
-      covers: response.books.covers
-    }).then((response) => {
-      for (var i = 0; i < response.books.length; i++) {
-        const id = response.books.goodreadsId;
-        const title = response.books.title;
+    $.get("/goodReads", function (data) {
+      let searchList = [];
+      let { data } = response.books;
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        searchList.push(cardDeck(data[i]));
       }
-      searchList.append(cardDeck(response))
-    })
-      .catch((err) => console.log(err));
+    }).catch((err) => console.log(err));
   };
 
-  //  need  call que  and create function by using carddeck.
+  // call que  and create function by using carddeck.
   function renderQueueList() {
-    $.get("/goodReads", {
-
-    }).then((response) => {
-      MyQueueList.append(cardDeck(response))
+    $.get("/api/myqueue", function (data) {
+      let MyQueueList = [];
+      for (var i = 0; i < data.length; i++) {
+        MyQueueList.push(cardDeckOfQList(data[i]));
+      }
     })
   }
-
-  //  need  call lib  and create function by using carddeck.
-  function renderLibraryList() {
-    $.get("/goodReads", {
-     
-    }).then((response) => {
-      MyLibraryList.append(cardDeck(response))
+  //call book  and create function by using carddeck.
+  function rendermyBookList() {
+    $.get("/api/mybooks", function (data) {
+      let MyBookList = [];
+      for (var i = 0; i < data.length; i++) {
+        MyBookList.push(cardDeck(data[i]));
+      }
     })
   }
-
-  // adding to the que
+  // add to the Quelist
   $(".addQue").on("click", function (event) {
     event.preventDefault();
-    $.post("/myqueue").then(function () {
-
+    $.post("/api/myqueue").then(function (data) {
+      return renderQueueList;
     })
   })
+//  add to the bookList
+  $(".done").on("click", function (event) {
+    event.preventDefault();
+    $.post("/api/mybooks").then(function (data) {
+      return rendermyBookList;
+    })
+  })
+
+  // delete from myQue
+  $(".deleteQue").on("click", function (event) {
+    event.preventDefault();
+    $.delete("/api/myqueue/:id").then(function (data) {
+      return renderQueueList
+    });
+  })
+//  delete from mybook
+  $(".deleteBook").on("click", function (event) {
+    event.preventDefault();
+    $.delete("/api/mybooks/:id").then(function (data) {
+      return rendermyBookList;
+    });
+  })
   // creating cardDeck 
-  function cardDeck(response) {
+  function cardDeck(data) {
     cardForm.append($("#books"));
     const cardForm = ` 
     <div style="width: 24rem;" class="card results-card">
     <div class="card-body">
-    <img src="${response.img}" alt="cover">
-    <h5 class="card-title">  ${response.title} </h5>
-    <h6 class="card-text"> ${response.author}  </h6>
+    <img src="${data.img}" alt="cover">
+    <h5 class="card-title">  ${data.title} </h5>
+    <h6 class="card-text"> ${data.author}  </h6>
     <button class="addQue">add</button>
+    <button class="deleteBook">delete</button> 
     `;
     return cardForm;
   }
-
+  function cardDeckOfQList(data) {
+    cardForm.append($("#books"));
+    const cardForm = ` 
+    <div style="width: 24rem;" class="card results-card">
+    <div class="card-body">
+    <img src="${data.img}" alt="cover">
+    <h5 class="card-title">  ${data.title} </h5>
+    <h6 class="card-text"> ${data.author}  </h6>
+    <button class="done">Done</button> 
+    <button class="deleteQue">delete</button> 
+    `;
+    return cardForm;
+  }
 
 
   // Submit the input by click or enter
@@ -72,7 +103,7 @@ $(document).ready(function () {
     console.log("asd");
     searchList.show()
     MyQueueList.hide()
-    MyLibraryList.hide()
+    MyBookList.hide()
     const searchInput = $(".searchInput").val().trim();
     serachByTitle(searchInput);
   });
@@ -80,18 +111,16 @@ $(document).ready(function () {
 
   myQueueBtn.on("click", () => {
     MyQueueList.show();
-    MyLibraryList.hid();
+    MyBookList.hid();
     console.log("asd");
     renderQueueList();
   });
   myLibraryBtn.on("click", () => {
-    MyLibraryList.show();
+    MyBookList.show();
     MyQueueList.hid();
     console.log("asd");
-    renderLibraryList();
+    rendermyBookList();
   });
-
-
 
 
   // Rendering Username on the homepage when user got logged in 
